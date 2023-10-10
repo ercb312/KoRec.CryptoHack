126ad59683e0fcc653e374811e4371f7ad39b441
변호준 <ericb312@korea.ac.kr>
2023-10-10 20:58:00 +0900

    LemurXOR explained

--- general/XOR/LemurXOR.py
+++ general/XOR/LemurXOR.py
@@ -3,20 +3,25 @@
 
 from PIL import Image;
 import numpy as np
-from pwn import *
 
-
-flag = Image.open('flag1.png')
+#PIL 모듈 사용
+flag = Image.open('flag1.png') 
 lemur = Image.open('lemur1.png')
 
+#rgb(#, #, #) => 배열 형식 
+#사진의 배열 받아오기
 flagarr = np.array(flag)
 lemurarr = np.array(lemur)
 
+#배열 xor 연산
 v = np.bitwise_xor(flagarr, lemurarr)
 
-# print(type(v))
+#np.bitwise_xor의 데이터 타입 확인
+print(type(v))
 
+#xor 연산이 된 배열을 이미지로 변환 
 flag2 = Image.fromarray(v)
+#이미지 출력
 flag2.show()
 
 
